84bb2bfa6c6505db7768bc288d57a39da98d3c85
Jannis R <mail@jannisr.de>
2017-05-05 15:47:27 +0200

    bugfixes :bug:, optimizations
    - optional return trip
    - follow redirects
    - fix ticket link scraping :bug:
    - remove optional query params
    - fix leg parsing :bug:

--- index.js
+++ index.js
@@ -11,9 +11,13 @@ const parse = require('./lib/parse')
 
 const endpoint = 'https://reiseauskunft.bahn.de/bin/query.exe/dn'
 
+const convertDate = (d) => {
+	return moment.tz(+d, 'Europe/Berlin').locale('de')
+}
+
 const link = (data) => {
 	const departure = moment.tz(+data.departure, 'Europe/Berlin').locale('de')
-	const _return = moment.tz(+data.return, 'Europe/Berlin').locale('de')
+	const _return = data.return ? convertDate(data.return) : null
 
 	const req = {
 		S: data.from.name,
@@ -22,30 +26,26 @@ const link = (data) => {
 		REQ0JourneyStopsZID: 'L=00' + data.to.id,
 		date: departure.format('dd, DD.MM.YY'),
 		time: departure.format('HH:mm'),
-		returnDate: _return.format('dd, DD.MM.YY'),
-		returnTime: _return.format('HH:mm'),
+		returnDate: _return ? _return.format('dd, DD.MM.YY') : '',
+		returnTime: _return ? _return.format('HH:mm') : '',
 		existOptimizePrice: '1',
 		country: 'DEU',
-		dbkanal_007: 'L01_S01_D001_KIN0001_qf-bahn-svb-kl2_lz03',
 		start: '1',
 		REQ0JourneyStopsS0A: '1',
 		timesel: 'depart',
 		returnTimesel: 'depart',
-		revia: 'yes',
 		optimize: '0',
 		auskunft_travelers_number: '1',
 		'tariffTravellerType.1': 'E',
 		'tariffTravellerReductionClass.1': '2',
 		tariffClass: '2',
-		rtMode: 'DB-HYBRID',
-		externRequest: 'yes',
-		HWAI: 'JS!js=yes!ajax=yes!'
+		rtMode: 'DB-HYBRID'
 	}
-	console.error(req)
 	const target = endpoint + '?' + qs.stringify(req)
 
 	return fetch(target, {
 		cache: 'no-store',
+		redirect: 'follow',
 		headers: {
 			'user-agent': 'https://github.com/derhuerst/generate-db-shop-urls'
 		}
