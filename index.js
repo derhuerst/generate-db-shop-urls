b4988e94e507a60d5fd21066d19815f4f3d9e1ca
Jannis R <mail@jannisr.de>
2017-05-05 17:40:57 +0200

    refactor requests

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 'use strict'
 
 const moment = require('moment-timezone')
-const qs = require('querystring')
-const {fetch} = require('fetch-ponyfill')()
-const cookie = require('cookie')
-const ct = require('content-type')
-const {decode} = require('iconv-lite')
 
+const request = require('./lib/request')
 const parse = require('./lib/parse')
-
-const endpoint = 'https://reiseauskunft.bahn.de/bin/query.exe/dn'
 
 const convertDate = (d) => {
 	return moment.tz(+d, 'Europe/Berlin').locale('de')
@@ -41,36 +35,8 @@ const link = (data) => {
 		tariffClass: '2',
 		rtMode: 'DB-HYBRID'
 	}
-	const target = endpoint + '?' + qs.stringify(req)
 
-	return fetch(target, {
-		cache: 'no-store',
-		redirect: 'follow',
-		headers: {
-			'user-agent': 'https://github.com/derhuerst/generate-db-shop-urls'
-		}
-	})
-	.then((res) => {
-		if (!res.ok) throw new Error('response not ok: ' + res.status)
-
-		// todo: parse cookies
-		// const cookies = {}
-		// res.headers.forEach((value, name) => {
-		// 	if (name === 'set-cookie') {
-		// 		Object.assign(cookies, cookie.parse(value))
-		// 	}
-		// })
-		// console.error(cookies)
-
-		const c = ct.parse(res.headers.get('content-type'))
-		if (c.parameters && c.parameters.charset) {
-			return res.buffer()
-			.then((raw) => decode(raw, 'ISO-8859-1'))
-		}
-
-		return res.buffer()
-		.then((raw) => raw.toString('utf8'))
-	})
+	return request('https://reiseauskunft.bahn.de/bin/query.exe/dn', req)
 	.then(parse(data))
 }
 
